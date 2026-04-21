2f2d8ef3efb2ffc9be76413adb1d55b3802167d8
= <s226313877@deakin.edu.au>
2026-04-21 16:42:46 +1000

    Document implemented features

--- diceRoller.cpp
+++ diceRoller.cpp
@@ -2,6 +2,10 @@
 #include <cstdlib>
 #include <ctime>
 
+/**
+ * @brief The menu options within the Roller class.
+ * 
+ */
 enum menu_options
 {
     ROLL_DIE = 1,
@@ -9,6 +13,11 @@ enum menu_options
     SEE_HISTORY,
     QUIT_APP
 };
+
+/**
+ * @brief A structure to hold the history of a die. Maximum length: 5.
+ * 
+ */
 struct die_history
 {
     int previous_rolls[5] = {-1, -1, -1, -1, -1};
@@ -43,6 +52,10 @@ struct die_history
     }
 };
 
+/**
+ * @brief A rollable die with history of the past (up to) 5 values. Maximum value can be set in the constructor.
+ * 
+ */
 class Die
 {
 protected:
@@ -50,11 +63,20 @@ protected:
     die_history _history;
 
 public:
+    /**
+     * @brief Construct a new Die object. Size will be 6.
+     * 
+     */
     Die()
     {
         Die(6);
     }
 
+    /**
+     * @brief Construct a new Die object.
+     * 
+     * @param size The maximum value on the die (6 gives a classic die).
+     */
     Die(int size)
     {
         if (size <= 1)
@@ -65,6 +87,11 @@ public:
         _maximum = size;
     }
 
+    /**
+     * @brief Rolls the die.
+     * 
+     * @return The value that the die rolled.
+     */
     int roll()
     {
         int value = (rand() % _maximum) + 1;
@@ -72,11 +99,22 @@ public:
         return value;
     }
 
+    /**
+     * @brief Get the history of the die.
+     * 
+     * @return The history of the die (up to 5 rolls).
+     */
     die_history get_history()
     {
         return _history;
     }
 
+    /**
+     * @brief Get an historical roll of the die. 
+     * 
+     * @param history_counter The number of values to go into the history. Throws an exception (string) if value does not exist. Minimum value: 1. Maimum value: min(5, _history.length).
+     * @return The historical roll of the die.
+     */
     int get_past_value(int history_counter)
     {
         if (history_counter > _history.length || history_counter <= 0)
@@ -86,12 +124,21 @@ public:
         return _history.previous_rolls[history_counter - 1];
     }
 
+    /**
+     * @brief Returns the type of the die (e.g. d4, d6)
+     * 
+     * @return The die type. 
+     */
     string die_type()
     {
-        return "D" + to_string(_maximum);
+        return "d" + to_string(_maximum);
     }
 };
 
+/**
+ * @brief A die with customisable faces. It can be set to have a custom minimum and maximum, and/or a custom step between faces, or six unique faces.
+ * 
+ */
 class CustomDie : public Die
 {
 private:
@@ -113,14 +160,23 @@ public:
     }
 };
 
+/**
+ * @brief Designed to hold the dice used in the program. Holds a d4, d6, d8, d10, d12, and a d20.
+ * 
+ */
 struct die_cup
 {
     Die dice[6] = {Die(4), Die(6), Die(8), Die(10), Die(12), Die(20)};
     int length = 6;
 };
-class Program
-{
 
+/**
+ * @brief The class that operates the dice roller. One public method: int main().
+ *
+ */
+class Roller
+{
+private:
     /**
      * @brief Uses SplashKit functions to repeatedly prompt the user for input until they provie a valid integer.
      *
@@ -176,6 +232,10 @@ class Program
         return candidate;
     }
 
+    /**
+     * @brief Prints the main menu of the dice roller app. Options are roll a die, edit the custom die, see the history of a die, and quit.
+     *
+     */
     void print_main_menu_options()
     {
         write_line();
@@ -186,6 +246,11 @@ class Program
         write_line("4: Quit");
     }
 
+    /**
+     * @brief Prints the dice in the given dice cup, including a numeric count.
+     *
+     * @param dice The dice to print.
+     */
     void print_dice(die_cup dice)
     {
         for (int i = 0; i < dice.length; i++)
@@ -193,6 +258,13 @@ class Program
             write_line("Die " + to_string(i + 1) + ": " + dice.dice[i].die_type());
         }
     }
+
+    /**
+     * @brief Rolls a user-chosen die from the die cup. Returns the value that the die rolls.
+     *
+     * @param dice The dice that the user can choose from to roll.
+     * @return What the die rolled.
+     */
     int roll_a_die(die_cup &dice)
     {
         print_dice(dice);
@@ -200,12 +272,17 @@ class Program
         Die *chosen_die = &dice.dice[chosen_die_index - 1];
         (*chosen_die).roll();
         // Future: If you ever need to do something to a die after rolling it, it can be done below.
-        // Techincally the last line could be returned, but I'm doing it separately.
+        // Techincally the line before this comment could be returned, but I'm doing it separately.
         int past_value = (*chosen_die).get_past_value(1);
 
         return past_value;
     }
 
+    /**
+     * @brief Prints the history of a user-chosen die from the die cup. Due to how the history struct is modified, shows a maximum of 5 values.
+     *
+     * @param dice The dice that the user can choose from to print the history of.
+     */
     void print_history(die_cup dice)
     {
         print_dice(dice);
@@ -224,6 +301,11 @@ class Program
     }
 
 public:
+    /**
+     * @brief Runs the dice roller.
+     *
+     * @return Status code.
+     */
     int main()
     {
         // Set up:
@@ -274,9 +356,15 @@ public:
         return 0;
     }
 };
+
+/**
+ * @brief The entry point of the program.
+ *
+ * @return Status code.
+ */
 int main()
 {
     srand(time(0));
-    Program program = Program();
-    program.main();
+    Roller roller = Roller();
+    return roller.main();
 }
