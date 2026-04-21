5ef7af25449551d6160246ce36ef6e6bc6907f4f
= <s226313877@deakin.edu.au>
2026-04-21 15:17:49 +1000

    User Interaction

--- diceRoller.cpp
+++ diceRoller.cpp
@@ -1,5 +1,12 @@
 #include "splashkit.h"
 
+enum menu_options
+{
+    ROLL_DIE = 1,
+    EDIT_CUSTOM,
+    SEE_HISTORY,
+    QUIT
+};
 struct die_history
 {
     int previous_rolls[5];
@@ -86,23 +93,149 @@ public:
     int roll() {}
 };
 
+struct die_cup
+{
+    Die dice[6] = {Die(4), Die(6), Die(8), Die(10), Die(12), Die(20)};
+    int length = 6;
+};
 class Program
 {
+
+    /**
+     * @brief Uses SplashKit functions to repeatedly prompt the user for input until they provie a valid integer.
+     *
+     * @param prompt The string to prompt the user with.
+     * @return The integer inputted by the user.
+     */
+    int input_int(string prompt)
+    {
+        string input = "";
+        write(prompt);
+        input = read_line();
+        while (!is_integer(input))
+        {
+            write_line("Your input of \"" + input + "\" is not a valid integer.");
+            write(prompt);
+        }
+
+        return to_integer(input);
+    }
+
+    /**
+     * @brief Uses SplashKit functions to repeatedly prompt the user for input until they provide a valid integer.
+     *
+     * @param prompt The string to prompt the user with.
+     * @param lower_bound The lowest value to be accepted (inclusive).
+     * @param upper_bound The highest value to be accepted (inclusive).
+     * @return The integer inputted by the user.
+     */
+    int input_int(string prompt, int lower_bound, int upper_bound)
+    {
+        if (lower_bound > upper_bound)
+        {
+            // Swap bounds as lower is greater than upper
+            int temp = upper_bound;
+            upper_bound = lower_bound;
+            lower_bound = temp;
+        }
+        int candidate;
+
+        do
+        {
+            candidate = input_int(prompt);
+            if (candidate < lower_bound || candidate > upper_bound)
+            {
+                write_line("Please input an integer between " +
+                           to_string(lower_bound) +
+                           " and " +
+                           to_string(upper_bound) +
+                           ".");
+            }
+        } while (candidate < lower_bound || candidate > upper_bound);
+
+        return candidate;
+    }
+
+    void print_main_menu_options()
+    {
+        write_line();
+        write_line("----------Main Menu----------");
+        write_line("1: Roll a die");
+        write_line("2: Edit the Custom Die (WIP)");
+        write_line("3: See history");
+        write_line("4: Quit");
+    }
+
+    int roll_a_die(die_cup &dice)
+    {
+        int chosen_die_index = input_int("Choose a die to roll: ", 1, dice.length);
+        Die *chosen_die = &dice.dice[chosen_die_index];
+
+        (*chosen_die).roll();
+        // Future: If you ever need to do something to a die after rolling it, it can be done below.
+        // Techincally the last line could be returned, but I'm doing it separately.
+        return (*chosen_die).get_past_value(0);
+    }
+
+    void print_history(die_cup dice)
+    {
+        int chosen_die_index = input_int("Choose a die to see the history of: ", 1, dice.length);
+        Die *chosen_die = &dice.dice[chosen_die_index];
+        die_history history = (*chosen_die).get_history();
+        string position_honorific[5] = {"st", "nd", "rd", "th", "th"};
+
+        write_line("History of the d" + to_string(chosen_die_index) + ":");
+        
+        for (int i = 0; i < history.length; i++)
+        {
+            write_line(to_string(i) + position_honorific[i] + ": " + to_string(history.previous_rolls[i]));
+        }
+        write_line("End history (max 5 values).");
+    }
+
     int main()
     {
         // Set up:
         // d4, d6, d8, d10, d12, d20, d100, custom.
-
+        die_cup dice;
+        int user_selection;
         // Begin app loop
-        // Choose option: roll die, edit custom, see history
-        // Choose die to roll, roll it
-        // Return its roll
-        // OR
-        // edit the custom die
-        // OR
-        // print history of the chosen die
+        do
+        {
+            user_selection = input_int("Menu option: ", 1, 4);
 
-        // When a specific die is accessed to roll, it will be stored in a pointer.
+            // Choose option: roll die, edit custom, see history, quit
+            // Choose die to roll, roll it
+            // Return its roll
+            // OR
+            // edit the custom die
+            // OR
+            // print history of the chosen die
+            // quit
+            switch (user_selection)
+            {
+            case ROLL_DIE:
+                write_line("------Roll a Die------");
+                int rolled = roll_a_die(dice);
+                write_line("You rolled a " + to_string(rolled));
+                break;
+            case EDIT_CUSTOM:
+                // Edit the custom die (WIP)
+                write_line("------Edit the Custom Die------");
+                write_line("Not yet implemented; no custom die yet");
+                break;
+            case SEE_HISTORY:
+                write_line("------See History------");
+                // See history of a die
+                break;
+            case QUIT:
+                // Do nothing, user wants to quit
+                break;
+            default:
+                break;
+            }
+        } while (user_selection != 4);
+
         return 0;
     }
 };
